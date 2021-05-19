213f0c396605ae73897550af2f070bdcb20a6e2b
rudra2027 <52098572+rudra2027@users.noreply.github.com>
2021-05-20 05:28:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import Main from './Main'
+
+
+function App() {
+    return ( <
+        div className = "App" >
+
+        <
+        h1 style = {
+            {
+                color: "black",
+                fontStyle: "italic"
+            }
+        } > < center > Welcome To The Portal For Counting Maxmum occurence Of Words IN A File < /center > < /
+        h1 > <
+        Main / >
+        <
+        /div>
+    );
+}
+
+export default App;
